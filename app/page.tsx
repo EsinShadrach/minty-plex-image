68cd8bb03d660a1b5f08b07f84f05f0abcf4ab77
EsinShadrach <esinshadrachrafe@gmail.com>
2023-12-07 23:57:26 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home({ searchParams }: HomeProps) {
 
   const { ref, isLoading, getPng } = useToImage({
     backgroundColor: "#2063F2",
+    quality: 1,
   });
 
   return (
--- custom.d.ts
+++ custom.d.ts
@@ -1,1 +1,21 @@
-declare module "@hcorta/react-to-image";
+declare module "@hcorta/react-to-image" {
+  export interface ToImageOptions {
+    width?: number | null;
+    height?: number | null;
+    backgroundColor?: string;
+    style?: React.CSSProperties;
+    quality?: number;
+    cacheBust?: boolean;
+    imagePlaceholder?: string;
+    pixelRatio?: number;
+    filter?: (node: HTMLElement) => boolean;
+  }
+
+  export interface ToImageResult {
+    ref: any;
+    isLoading: boolean;
+    getPng: () => void;
+  }
+
+  export function useToImage(options: ToImageOptions): ToImageResult;
+}
